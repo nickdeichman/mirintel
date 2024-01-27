59db03afeda33ec725370b23f3a62eabcdd62df7
unknown <nexloder05@gmail.com>
2024-01-27 11:59:50 +0200

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  basename: process.env.NODE_ENV === 'production' ? '/mirintel/' : '/',
+  basename: '/',
   plugins: [react()],
   build: {
     outDir: './build'
